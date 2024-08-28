50452e9e485bb479e54c94de19b715d1280c8762
UNIVERSECOD <leylaha@code.edu.az>
2024-08-28 08:47:48 +0400

    Local storage order page adding and removing

--- home page/index.js
+++ home page/index.js
@@ -22,9 +22,11 @@ function fillTable() {
  }
  
  function addToCart(product) {
-     console.log(product);
+     const cart = JSON.parse(localStorage.getItem("cart")) ?? [];
+     cart.push(product);
+     localStorage.setItem("cart", JSON.stringify(cart));
      
- }
+ } 
 /*  <div class="table-data"></div>*/
 
 fillTable();
--- orders/index.js
+++ orders/index.js
@@ -1,7 +1,7 @@
 const tableContent = document.querySelector(".table-content")
 function fillTable() {
+    const products = getCartItems ();
     products.forEach((product) => {
-
         const rowElement = document.createElement("div")
         rowElement.className = "table-row";
         rowElement.innerHTML = `
@@ -10,22 +10,47 @@ function fillTable() {
          <div class="table-data">${product.description}</div>
          <div class="table-data">${product.price}</div>
          <div class="table-data"></div>
-         <div class="table-data"><img width="40" src="../assets/shopping-cart.svg" alt=""></div>
+         <div class="table-data"><img width="20" src="../assets/trash.svg" alt=""></div>
          `;
 
-
-        rowElement.querySelector("img").addEventListener("click", () => addToCart(product));
+         rowElement.querySelector("img").addEventListener("click", () => {
+            removeFromCart(product);
+            rowElement.remove();
+    });
 
         tableContent.append(rowElement);
-
-
+        
     });
+
+function removeFromCart(product) {
+    const cart = getCartItems();
+    const productIdx = cart.findIndex((p)=> p.id === product.id);
+    if(productIdx !== -1){
+        cart.splice(productIdx, 1);
+        setCart(cart);
+    }
 }
 
-function addToCart(product) {
-    console.log(product);
+
+function getCartItems(){
+    return JSON.parse(localStorage.getItem("cart")) ?? [];
     
 }
 
+function setCart(cart){
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 fillTable();
+
+
+
+
+     
+    // // localStorage.setItem("product", JSON.stringify([product2]))
+    // const productJson = localStorage.getItem("products");
+    // const products = JSON.parse(productJson);
+    // console.log(products);
+    // products.push(product2);
+    // localStorage.setItem("products", JSON.stringify(products));
+
